063287901685514ab6813a443e38ca34511a3f4c
courageousstrawberry <elech2a@gmail.com>
2024-11-15 18:57:34 -0800

    begin main

---
+++ 210-lab-32.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Car.h"
+#include <deque>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    deque<Car> booth(2);
+
+    Car one;
+    Car two;
+    booth.push_back(one);
+    booth.push_back(two);
+
+    while(!(booth.empty())) {
+        Car add
+    }
+
+    return 0;
+}
