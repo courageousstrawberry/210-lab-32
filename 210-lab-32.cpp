5addf7b2f75f929bb7b56254f307819d750b8473
courageousstrawberry <elech2a@gmail.com>
2024-11-15 19:40:03 -0800

    add comments

--- 210-lab-32.cpp
+++ 210-lab-32.cpp
@@ -17,24 +17,27 @@ int main() {
         car.print();
     }
 
-    
+    // Loop until all cars have payed tolls
     while(!(booth.empty())) {
         count++;
         cout << "Time: " << count;
-        int rand_num = 1 + rand() % 100;
+        int rand_num = 1 + rand() % 100; // Random probability number
 
+        // If probability is 55%, the front car pays it's toll
         if (rand_num <= 55){
             cout << " Operation: Car paid: ";
             booth.front().print();
             booth.pop_front();
         }
         else {
+            // Otherwise, the last car enters the lane
             Car add;
             booth.push_back(add);
             cout << " Operation: Joined lane: ";
             booth.back().print();
         }
 
+        // Display the queue of cars
         cout << "Queue: " << endl;
         if (booth.empty()) {
             cout << "Empty" << endl;
